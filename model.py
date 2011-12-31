14120c598c7b03dcfc385f2fa2d8a6eff29981d6
Aron Asor <aronasorman@gmail.com>
2011-12-31 21:41:03 +0800

    completed Counselor model and partially completed Section model  On branch master 	new file:   model.py

---
+++ model.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, Text, Date, LargeBinary, create_engine, Float
+from sqlalchemy.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, backref
+
+Base = declarative_base()
+
+engine = create_engine('sqlite://test.db', echo=True)
+
+session = sessionmaker(bind=engine)
+
+class Counselor(Base):
+    __tablename__ = "counselors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    picture = Column(LargeBinary)
+    nickname = Column(String(20))
+    address = Column(String, nullable=False)
+    telno = Column(String(20))
+    celno = Column(String(20))
+    email = Column(String(40))
+    birthdate = Column(Date, nullable=False)
+    position = Column(String(15), default='Counselor')
+    religion = Colum(String(20), nullable=False)
+    height = Column(Float, nullable=False)
+    weight = Column(Float, nullable=False)
+    overall_health = Column(String(1))
+    eyesight = Column(String(1), nullable=False)
+    hearing = Column(String(1), nullable=False)
+    lungs = Column(String(1), nullable=False)
+    handicaps = Column(Text)
+
+    sections = relationship('Section', order_by='Section.name', backref='user')
+
+class Section(Base):
+    __tablename__ = 'section_assignments'
+
+    name = Column(String(2), primary_key=True)
+    counselor_id = Column(Integer, ForeignKey('Counselor.id'))
+
+    counselor = relationship('Counselor', backref=backref('sections'))
