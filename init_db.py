ef430536a4bdbb5916a3537103857380f08d4811
Aron Asor <aronasorman@gmail.com>
2012-01-15 16:20:47 +0800

    added initializers for the various lookup tables. However, there's no sqlite db file appearing...  On branch master 	modified:   init_db.py

--- init_db.py
+++ init_db.py
@@ -10,4 +10,74 @@ from model import ParentStatus, SingleParent, LivingWith, StudyLength
 
 engine = create_engine('sqlite:///test.db', echo=True)
 
-session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
+
+def init_parent_status():
+    session = Session()
+    session.add_all([
+        ParentStatus("Living Together")
+        , ParenStatus('Separated')
+        , ParentStatus('Remarried')
+        , ParentStatus('Deceased - Mother')
+        , ParentStatus('Deceased - Father')
+        , ParentStatus('Deceased - Both')
+        ])
+    session.commit()
+
+def init_single_parent():
+    session = Session()
+    session.add_all([
+        SingleParent('Mother')
+        , SingleParent('Father')
+        , SingleParent('None')
+        ])
+    session.commit()
+
+def init_living_with():
+    session = Session()
+    session.add_all([
+        LivingWith('Both Parents')
+        , LivingWith('Mother')
+        , LivingWith('Father')
+        , LivingWith('Guardian')
+        ])
+    session.commit()
+
+def init_study_length():
+    session = Session()
+    session.add_all([
+        StudyLength('1-3 hours')
+        , StudyLength('4-5 hours')
+        , StudyLength('more than 5 hours')
+        ])
+    session.commit()
+
+def init_subjects():
+    session = Session()
+    session.add_all([
+        Subject('Filipino')
+        , Subject('English')
+        , Subject('Science')
+        , Subject('Social Science')
+        , Subject('Theology')
+        , Subject('Chemistry')
+        , Subject('Biology')
+        , Subject('Physics')
+        , Subject('Algebra')
+        , Subject('History')
+        , Subject('Computer Science')
+        , Subject('Home Economics')
+        , Subject('Physical Education')
+        , Subject('Philosophy')
+        , Subject('Geometry')
+        , Subject('Calculus')
+        , Subject('Journalism')
+        ])
+    session.commit()
+
+def db_init():
+    init_parent_status()
+    init_single_parent()
+    init_living_with()
+    init_study_length()
+    init_subjects()
