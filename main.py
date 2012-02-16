f82d3ada7cdb0a06f15a4c1906227fb6c742c668
Aron Asor <aronasorman@gmail.com>
2012-02-16 12:19:08 +0800

    fixed login and account creation logic. On to the main page!  On branch master 	modified:   main.py

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ import os
 
 from misc_models import *
 from model import *
+from utils import to_date
 
 urls = (
     '/', 'login'
     , '/create-account', 'accountcreation'
+    , '/main', 'mainpage'
     )
 
 DBNAME = 'counselor.db'
@@ -24,7 +26,6 @@ DBPATH = os.getcwd()
 
 engine = create_engine('sqlite:////' + os.path.join(DBPATH,DBNAME), echo=True)
 Session = sessionmaker(bind=engine)
-db_session = Session()
 
 app =  web.application(urls, globals())
 render = web.template.render('templates/')
@@ -33,35 +34,30 @@ initializer = {
     }
 session = web.session.Session(app, web.session.DiskStore('sessions'), initializer = initializer)
 
-loginform = form.Form(
-    form.Textbox('username')
-    , form.Password('password')
-    , form.Button('login')
-)
-
 class login:
     '''
     Class that handles logging in.
     '''
     
     def GET(self):
-        f = loginform()
-        return render.login(f)
+        return render.login(None)
     def POST(self):
-        f = loginform()
-        if f.validates():
-            data = web.input()
-            password_hash = sha256(data['password'])
-            userlist = [ row.User for row in db_session.query(User, User.password).all() if row.password == password_hash]
-            if userlist:
-                user = userlist[0]
-                session.user = user
-                raise web.redirect('/index')
-            else:
-                return render.login(f)
+        data = web.input()
+        try:
+            name = int(data['username'])
+        except ValueError:
+            return render.login("Please enter a number as the username.")
         else:
-            render.login(f)
-
+            password_hash = sha256(data['password']).hexdigest()
+            db_session = Session()
+            import pdb; pdb.set_trace()
+            user = db_session.query(User).filter(User.id==name).filter(User.password==password_hash).first()
+            if user:
+                session.user = user
+                raise web.seeother('/mainpage')
+            else:
+                return render.login('Username or password is incorrect.')
+            
 class accountcreation:
     '''
     Handler for account creation
@@ -69,6 +65,32 @@ class accountcreation:
     def GET(self):
         return render.createaccount()
 
+    def POST(self):
+        
+        db_session = Session()
+        data = web.input()
+        user = User()
+        user.id = int(data['ID Number'])
+        user.password = sha256(data['Password']).hexdigest()
+        db_session.add(user)
+        db_session.commit()
+
+        db_session = Session()
+        counselor = Counselor()
+        counselor.id = user.id
+        name = ' '.join([data['firstname'], data['middleinitial'], data['lastname']])
+        counselor.name = name
+        counselor.nickname = data['Nickname']
+        counselor.address = data['City Address']
+        counselor.telno = data['telephone']
+        counselor.celno = data['cellphone']
+        counselor.email = data['email']
+        counselor.is_head_counselor = False
+        db_session.add(counselor)
+        db_session.commit()
+
+        return "success!"
+        
 class index:
     '''
     Class that handles the homepage once a person has logged in.
