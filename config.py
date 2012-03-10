44b51d498667ddbb91b871dcbc65ed5ec2b9aab6
Aron Asor <aronasorman@gmail.com>
2012-03-10 11:11:54 +0800

    changed period generation algo to generate $period_span days back and ahead, and turned the period generation functions into generators and added the periods one by one to avoid space leaks  On branch master 	modified:   config.py 	modified:   generate_period.py

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ COUNSELOR_NUM_WORK_DAYS=5
 period_labels = ['1st period', '2nd period', '3rd period', '4th period',
                  '5th period (1)', '5th period (2)', '6th period', '7th period', '8th period']
 
+period_span = 3000 # days
+
--- generate_period.py
+++ generate_period.py
@@ -16,8 +16,16 @@ def dates_of_current_week():
     dates = [today - timedelta(day_of_week - x) for x in range(COUNSELOR_NUM_WORK_DAYS)]
     return dates
 
+def generate_date_span():
+    earliest_date = date.today() - timedelta(period_span/5) # not too far into the past...
+    latest_date = date.today() + timedelta(period_span)
+    date_ctr = earliest_date
+    while date_ctr <= latest_date:
+        yield date_ctr
+        date_ctr += timedelta(1)
+
 def generate_periods():
-    return [Period(num=num,date=date) for date in dates_of_current_week() for num in range(NUM_OF_PERIODS)]
+    return (Period(num=num,date=date) for date in generate_date_span() for num in range(NUM_OF_PERIODS))
 
 def create_periods():
     periods = generate_periods()
