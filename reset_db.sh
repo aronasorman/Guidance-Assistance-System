16d4fb46a9584b38ba13fe0fd990cb9f7b179497
Aron Asor <aronasorman@gmail.com>
2012-03-17 01:17:00 +0800

    now using the complete registrar data  On branch master 	new file:   00-xxx.csv 	modified:   reset_db.sh

--- reset_db.sh
+++ reset_db.sh
@@ -8,5 +8,5 @@ fi
 
 bin/python init_db.py
 bin/python generate_period.py
+bin/python import_registrar_csv.py 00-xxx.csv
 bin/python test_data.py
-bin/python import_registrar_csv.py registrar-sample-data.txt
